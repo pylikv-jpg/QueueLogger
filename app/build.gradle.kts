9bc8cddeb87d6ad1972bbce551e26f50ee320e8b
pylikv-jpg <pylik.v@gmail.com>
2026-08-30 11:44:46 +0300

    Increment versionCode from 1 to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.pylikv.queuelogger"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
